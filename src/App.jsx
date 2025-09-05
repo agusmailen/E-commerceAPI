209f3cfb72a4236c0073b9c2f0eb00eb4e053479
Matias Krivitzki <makri_2014@hotmail.com>
2025-09-05 16:12:48 -0300

    Cambio para crear carpeta Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
-import Breadcrumb from './components/Breadcrumb';
-import ProductGallery from './components/ProductGallery';
-import ProductInfo from './components/ProductInfo';
-import QuantitySelector from './components/QuantitySelector';
-import AddToCartButton from './components/AddToCartButton';
-import './App.css';
+import ProductInfoPage from './pages/ProductInfo';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main className="main-content">
-        <Breadcrumb />
-        <section className="product-section">
-          <div className="product-container">
-            <ProductGallery />
-            <div className="product-info-wrapper">
-              <ProductInfo />
-              <div className="add-to-cart-section">
-                <div className="quantity-cart-wrapper">
-                  <QuantitySelector />
-                  <AddToCartButton />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-    </>
-  );
+  return <ProductInfoPage />;
 }
 
 export default App;
