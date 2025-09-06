c58a44898702407183cea2a9024ed669349eec1a
Juan Ignacio Molina <juanignaciomoli@gmail.com>
2025-09-06 11:44:01 -0300

    Adding login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import ProductList from './pages/ProductList/index.jsx';
 import ProductInfoPage from './pages/ProductInfo';
+import Login from './pages/Login';
 
 export const App = () => {
   return (
     <>
-      {/* <ProductInfoPage /> */}
+      {/* Switch views by commenting/uncommenting the lines below */}
+      
+      {/* Home/Product List View */}
       <ProductList />
+      
+      {/* Login View */}
+      {/* <Login /> */}
+      
+      {/* Product Info View */}
+      {/* <ProductInfoPage /> */}
     </>
   );
 }
