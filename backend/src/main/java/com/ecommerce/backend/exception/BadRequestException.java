c2a9fc7bd33025477b41e2563cc1eb4e9b1c936b
unknown <marrapodiagustina@gmail.com>
2025-10-12 20:09:01 -0300

    elimina metodo duplicado

--- backend/src/main/java/com/ecommerce/backend/exception/BadRequestException.java
+++ backend/src/main/java/com/ecommerce/backend/exception/BadRequestException.java
@@ -5,8 +5,4 @@ public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
     }
-
-    public BadRequestException(String id) {
-        super("", id);
-    }
 }
