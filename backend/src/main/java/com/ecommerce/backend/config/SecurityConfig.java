f17594261515f32df70e1897fe0a903891e07402
Juan Ignacio Molina <juanignaciomoli@gmail.com>
2025-10-22 19:32:40 -0300

    merge upstream

--- backend/src/main/java/com/ecommerce/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ecommerce/backend/config/SecurityConfig.java
@@ -45,7 +45,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-<<<<<<< HEAD
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
@@ -56,16 +55,6 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-=======
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/auth/*").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/productos").permitAll()
-
-
-
-                        .anyRequest().authenticated());
->>>>>>> 3ffb1687824d152f2e3f09ac3047b79b44fbcc84
 
         return http.build();
     }
